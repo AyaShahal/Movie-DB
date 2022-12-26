5e1dbb273709ee598ea8d97f2e2649db0ba61cc7
Ayashahl <ayashahal055@gmail.com>
2022-12-26 19:21:24 +0200

    step 4

--- index.js
+++ index.js
@@ -11,6 +11,29 @@ app.get("/test",(req, res)=>{
     app.get("/:time",(req, res) => {
     res.send({status:200,message:time})
     });
+
+    app.get("/hello/:id",(req,res) => {
+        res.send({status:200,message:req.params.id})
+        });
+        app.get("/search",(req,res) => {
+            const search = req.query.s;
+            
+            if (typeof search != 'undefined') {
+            const response = {
+            status:200, message:"ok", data: search
+            };
+            
+            res.send(response);
+            }
+            else {
+            const response = {
+            status:500, error:true, message: "you have to provide a search"
+            };
+            res.status(500);
+            res.send(response);
+            }
+            });
+        
 app.listen(port, function () {
     console.log(`Example app listening on port ${port}!`);
     });
