259c1910d24e34b4184bf5d6f47b405890c54637
Ayashahl <ayashahal055@gmail.com>
2022-12-26 19:16:45 +0200

    step 3

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const port = 3000;
 app.get("/", function (req, res) {
 res.send("ok");
 });
+app.get("/test",(req, res)=>{
+    res.send({status:200, message:"ok"})
+    });
+    app.get("/:time",(req, res) => {
+    res.send({status:200,message:time})
+    });
 app.listen(port, function () {
     console.log(`Example app listening on port ${port}!`);
     });
